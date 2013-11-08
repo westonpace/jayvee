42cd9a6b8d33be19a5fd003fbd26c5b110ccdc25
Weston Pace <weston.pace@gmail.com>
2013-11-08 08:53:36 -0700

    Forgot to commit the standard worker

---
+++ jayvee-core/src/main/java/com/github/westonpace/jayvee/workflow/StandardWorker.java
@@ -0,0 +1,28 @@
+package com.github.westonpace.jayvee.workflow;
+
+/**
+ * A standard worker implementation which provides logic for signalling when a worker
+ * has ended.
+ */
+public abstract class StandardWorker implements Worker {
+
+	private boolean ended = false;
+	
+	@Override
+	public boolean isEnded() {
+		return ended;
+	}
+	
+	/**
+	 * <p>
+	 * Ends this worker.  Called by subclasses when they can no longer iterate.
+	 * </p><p>
+	 * Most classes will not need to implement this.
+	 * </p><p>
+	 * @see Worker#isEnded
+	 * </p>
+	 */
+	protected void end() {
+		ended = true;
+	}
+}
